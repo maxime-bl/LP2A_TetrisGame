486c7a71306fbc95a7d8d3d337593f029887c431
HeosFx <flaviantheurel@outlook.fr>
2022-04-12 16:05:06 +0200

    Ajout constructeurs Tetrominos (a vérifier)

--- tetris_game/TetJ.java
+++ tetris_game/TetJ.java
@@ -1,5 +1,28 @@
 package tetris_game;
 
-public class TetJ {
+import java.awt.Color;
 
+public class TetJ extends Tetromino {
+	
+	public TetJ() {
+		super(Color.BLUE);
+		
+	}
+	
+	@Override
+	protected boolean hasCollided() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void rotateLeft() {
+		
+	}
+	
+	@Override
+	protected void rotateRight() {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- tetris_game/Tetromino.java
+++ tetris_game/Tetromino.java
@@ -9,12 +9,23 @@ public abstract class Tetromino {
 	protected Color color;
 	protected FallingTile centerTile;
 	
+	public Tetromino() {
+		this.tiles = new ArrayList<FallingTile>();
+		this.color = Color.BLACK;
+		centerTile = new FallingTile();
+	}
+	
+	public Tetromino(Color color) {
+		this.tiles = new ArrayList<FallingTile>();
+		this.color = color;
+		this.centerTile = new FallingTile(color);
+	}
+	
 	public Color getColor() {
 		return this.color;
 	}
 	
 	protected abstract boolean hasCollided();
-	
 	protected abstract void rotateLeft();
 	protected abstract void rotateRight();
 	
