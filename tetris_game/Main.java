7e5c6c6a665c3f24e128a238ed6a504e36376b6d
maxime-bl <maxime.blanchard19@gmail.com>
2022-04-13 10:59:39 +0200

    Create Main.java

---
+++ tetris_game/Main.java
@@ -0,0 +1,36 @@
+package tetris_game;
+
+import java.awt.Color;
+
+import processing.core.*;
+import scenes.Scene;
+import tiles.FallingTile;
+import tiles.Tile;
+
+public class Main extends PApplet{
+	private int windowWidth = 1280, windowHeight = 720;
+	private Scene currentScene;
+
+	public static void main(String[] args) {
+		PApplet.main("tetris_game.Main");
+	}
+
+	//function that is called before creating the window (PApplet)
+	public void settings() {
+		size(windowWidth, windowHeight);
+	}
+	
+	//called once after the PApplet is created
+	public void setup() {
+		
+	}
+	
+	//called every 1/60 of second
+	public void draw() {
+
+	}
+	
+	public void setCurrentScene(Scene scene) {
+		this.currentScene = scene;
+	}
+}
