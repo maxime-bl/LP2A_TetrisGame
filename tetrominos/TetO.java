3d562dda395aa246c05f698a9bf8a079de93e6ae
HeosFx <flaviantheurel@outlook.fr>
2022-04-15 13:37:48 +0200

    Ajout centerTile à tiles

--- tetrominos/TetO.java
+++ tetrominos/TetO.java
@@ -7,6 +7,7 @@ public class TetO extends Tetromino {
 
 	public TetO() {
 		super(ColorConstants.YELLOW, SpawningCoord.x, SpawningCoord.y);
+		super.tiles.add(centerTile);
 		super.tiles.add(new FallingTile(ColorConstants.YELLOW, SpawningCoord.x+1, SpawningCoord.y+1));
 		super.tiles.add(new FallingTile(ColorConstants.YELLOW, SpawningCoord.x+1, SpawningCoord.y));
 		super.tiles.add(new FallingTile(ColorConstants.YELLOW, SpawningCoord.x, SpawningCoord.y+1));
