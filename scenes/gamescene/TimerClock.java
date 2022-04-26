a4e57dbe23b52d766ca4e38574e93ac08e1b45d4
maxime-bl <maxime.blanchard19@gmail.com>
2022-04-26 15:17:42 +0200

    Commentaires TimerClock

--- scenes/gamescene/TimerClock.java
+++ scenes/gamescene/TimerClock.java
@@ -15,6 +15,9 @@ public class TimerClock {
 		
 	}
 	
+	/**
+	 * Makes the timer update its elapsed time (if it is not paused or finished)
+	 */
 	public void update() {
 		if (!isFinished && !isPaused) {
 			elapsedMillis += System.currentTimeMillis() - lastMillis;
@@ -22,6 +25,10 @@ public class TimerClock {
 		}
 	}
 	
+	/**
+	 * Sets the timer state to paused or unpaused, which prevents it from updating its elapsed time
+	 * @param isPaused
+	 */
 	public void setPaused(boolean isPaused) {
 		this.isPaused = isPaused;
 		if(isPaused == false) {
@@ -29,18 +36,32 @@ public class TimerClock {
 		}
 	}
 
+	/**
+	 * Gets the number of fully elapsed minutes since the timer started
+	 * @return the number of elapsed minutes
+	 */
 	public long getMinutes() {
 		return (long)Math.floor((elapsedMillis/1000)/60);
 	}
 	
+	/**
+	 * Gets the number of elapsed seconds since the timer started
+	 * @return the number of elapsed seconds
+	 */
 	public long getSeconds() {
 		return (elapsedMillis/1000)%60;
 	}
 	
-	public void setFinish(boolean b) {
-		this.isFinished = b;
+	/**
+	 * Sets the state of the timer to finished, which is irreversible and prevents it from updating its elapsed time
+	 */
+	public void setFinished() {
+		this.isFinished = false;
 	}
 	
+	/*
+	 * Sets the elapsed time back to zero
+	 */
 	public void reset() {
 		elapsedMillis = 0;
 		lastMillis = System.currentTimeMillis();
@@ -62,14 +83,26 @@ public class TimerClock {
 		return timer;
 	}
 	
+	/**
+	 * Returns the current state of the timer
+	 * @return true if the timer isn't paused or finished, false otherwise
+	 */
 	public boolean isRunning() {
 		return !isPaused && !isFinished;
 	}
 
+	/**
+	 * Returns the time between the timer initialization and its last update
+	 * @return the elapsed time in milliseconds
+	 */
 	public long getElapsedTime() {
 		return elapsedMillis;
 	}
 	
+	/**
+	 * Sets the elapsed time of the timer to the wanted value
+	 * @param elapsedMillis the elapsed time in milliseconds
+	 */
 	public void setElapsedTime(long elapsedMillis) {
 		this.elapsedMillis = elapsedMillis;
 	}
